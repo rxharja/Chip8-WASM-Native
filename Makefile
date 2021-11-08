cfb9d92252f72e79df3bde10d399d5dad36a2c27
Redon Xharja <redon.xharja@gmail.com>
2021-11-07 23:27:27 -0500

    webassembly optimizations

--- Makefile
+++ Makefile
@@ -2,50 +2,50 @@ INCLUDES=-I /usr/local/include/SDL2 -I ./include
 FLAGS=-g -D_GNU_SOURCE=1 -D_THREAD_SAFE
 OBJECTS=./build/chip8_memory.o ./build/chip8_stack.o ./build/chip8_keyboard.o ./build/chip8.o ./build/chip8_screen.o ./build/toot.o
 WASM_OBJECTS=./build/chip8_memory.html ./build/chip8_stack.html ./build/chip8_keyboard.html ./build/chip8.html ./build/chip8_screen.html ./build/toot.html
-EMCC_OPTIONS=-s WASM=1 -s USE_SDL=2 
+EMCC_OPTIONS=-s WASM=1 -s USE_SDL=2 -s EXPORT_ES6=1 -s MODULARIZE=1 
 TESTS=./tests/chip8_keyboard_tests.o
 
 all: ${OBJECTS} 
-	gcc ${FLAGS} ${INCLUDES} ${OBJECTS} ./src/main.c -L /usr/local/lib -o ./bin/Chip8 -lSDL2main -lSDL2
+	gcc -O3 ${FLAGS} ${INCLUDES} ${OBJECTS} ./src/main.c -L /usr/local/lib -o ./bin/Chip8 -lSDL2main -lSDL2
 
 ./build/chip8_memory.o:src/chip8_memory.c
-	gcc ${FLAGS} ${INCLUDES} ./src/chip8_memory.c -c -o ./build/chip8_memory.o
+	gcc -O3 ${FLAGS} ${INCLUDES} ./src/chip8_memory.c -c -o ./build/chip8_memory.o
 	
 ./build/chip8_stack.o:src/chip8_stack.c
-	gcc ${FLAGS} ${INCLUDES} ./src/chip8_stack.c -c -o ./build/chip8_stack.o
+	gcc -O3 ${FLAGS} ${INCLUDES} ./src/chip8_stack.c -c -o ./build/chip8_stack.o
 
 ./build/chip8_keyboard.o:src/chip8_keyboard.c
-	gcc ${FLAGS} ${INCLUDES} ./src/chip8_keyboard.c -c -o ./build/chip8_keyboard.o
+	gcc -O3 ${FLAGS} ${INCLUDES} ./src/chip8_keyboard.c -c -o ./build/chip8_keyboard.o
 
 ./build/chip8.o:src/chip8.c
-	gcc ${FLAGS} ${INCLUDES} ./src/chip8.c -c -o ./build/chip8.o
+	gcc -O3 ${FLAGS} ${INCLUDES} ./src/chip8.c -c -o ./build/chip8.o
 
 ./build/chip8_screen.o:src/chip8_screen.c
-	gcc ${FLAGS} ${INCLUDES} ./src/chip8_screen.c -c -o ./build/chip8_screen.o
+	gcc -O3 ${FLAGS} ${INCLUDES} ./src/chip8_screen.c -c -o ./build/chip8_screen.o
 
 ./build/toot.o:src/toot.c
-	gcc ${FLAGS} ${INCLUDES} ./src/toot.c -c -o ./build/toot.o
+	gcc -O3 ${FLAGS} ${INCLUDES} ./src/toot.c -c -o ./build/toot.o
 
 web: ${WASM_OBJECTS}
-	emcc ${FLAGS} ${INCLUDES} ${WASM_OBJECTS} ${EMCC_OPTIONS} ./src/main.c -L /usr/local/lib -o ./bin/Chip8.html --preload-file PONG2
+	emcc ${FLAGS} ${INCLUDES} ${WASM_OBJECTS} ${EMCC_OPTIONS} ./src/main.c -o ./bin/Chip8.html --preload-file ./bin/c8games/ --use-preload-plugins
 
 ./build/chip8_memory.html:src/chip8_memory.c
-	emcc ${FLAGS} ${INCLUDES} ./src/chip8_memory.c -c -o ./build/chip8_memory.html
+	emcc -O3 ${FLAGS} ${INCLUDES} ./src/chip8_memory.c -c -o ./build/chip8_memory.html
 	
 ./build/chip8_stack.html:src/chip8_stack.c
-	emcc ${FLAGS} ${INCLUDES} ./src/chip8_stack.c -c -o ./build/chip8_stack.html
+	emcc -O3 ${FLAGS} ${INCLUDES} ./src/chip8_stack.c -c -o ./build/chip8_stack.html
 
 ./build/chip8_keyboard.html:src/chip8_keyboard.c
-	emcc ${FLAGS} ${INCLUDES} ./src/chip8_keyboard.c -c -o ./build/chip8_keyboard.html
+	emcc -O3 ${FLAGS} ${INCLUDES} ./src/chip8_keyboard.c -c -o ./build/chip8_keyboard.html
 
 ./build/chip8.html:src/chip8.c
-	emcc ${FLAGS} ${INCLUDES} ./src/chip8.c -c -o ./build/chip8.html
+	emcc -O3 ${FLAGS} ${INCLUDES} ./src/chip8.c -c -o ./build/chip8.html
 
 ./build/chip8_screen.html:src/chip8_screen.c
-	emcc ${FLAGS} ${INCLUDES} ./src/chip8_screen.c -c -o ./build/chip8_screen.html
+	emcc -O3 ${FLAGS} ${INCLUDES} ./src/chip8_screen.c -c -o ./build/chip8_screen.html
 
 ./build/toot.html:src/toot.c
-	emcc ${FLAGS} ${INCLUDES} ./src/toot.c -c -o ./build/toot.html
+	emcc -O3 ${FLAGS} ${INCLUDES} ./src/toot.c -c -o ./build/toot.html
 
 test:
 	echo "Running Tests"
--- src/main.c
+++ src/main.c
@@ -5,28 +5,22 @@
 
 #ifdef __EMSCRIPTEN__
 #include <emscripten/emscripten.h>
-#include <SDL.h>
-#else
-#include <SDL.h>
+#include <emscripten/HTML5.h>
 #endif
 
+#include <SDL.h>
 #include "chip8.h"
 #include "chip8_keyboard.h"
 #include "chip8_screen.h"
 
 /*
- * Emscripten requires arguments to be passed in one parameter
- * A way around that was to create a struct with the chip8 and renderer
- */
-struct args {
-  struct Chip8 *chip8;
-  SDL_Renderer *renderer;
-};
-
-/*
  * Keyboard map mapping the virtual keyboard 
  * to the modern ones
  */
+struct Chip8 chip8;
+
+SDL_Renderer *renderer;
+
 const char keyboard_map[CHIP8_TOTAL_KEYS] = {
   SDLK_0, SDLK_1, SDLK_2, SDLK_3, 
   SDLK_4, SDLK_5, SDLK_6, SDLK_7, 
@@ -39,17 +33,12 @@ static void renderLoop();
 int main(int argc, const char **argv)
 {
 
-#ifdef __EMSCRIPTEN__
-  const char* filename = "PONG2";
-#else
   if (argc < 2) {
     printf("You must provide a file to load.\n");
     return -1;
   }
 
   const char* filename = argv[1];
-#endif
-
 
   printf("The file name to load is: %s\n", filename);
 
@@ -82,12 +71,11 @@ int main(int argc, const char **argv)
     return -1;
   }
 
-  struct Chip8 chip8;
   chip8_init(&chip8);
   chip8_load(&chip8, buf, size);
   chip8_keyboard_set_map(&chip8.keyboard, keyboard_map);
-  SDL_Init(SDL_INIT_EVERYTHING);
 
+  SDL_Init(SDL_INIT_EVERYTHING);
   SDL_Window *window = SDL_CreateWindow(
       EMULATOR_WINDOW_TITLE,
       SDL_WINDOWPOS_UNDEFINED,
@@ -97,39 +85,37 @@ int main(int argc, const char **argv)
       SDL_WINDOW_SHOWN
   );
 
-  SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_TEXTUREACCESS_TARGET);
+  renderer = SDL_CreateRenderer(window, -1, SDL_TEXTUREACCESS_TARGET);
 
-  
-  struct args args = { &chip8, renderer };
-  
 #ifdef __EMSCRIPTEN__
-  emscripten_set_main_loop_arg(renderLoop, &args, 0, 1);
+  emscripten_set_main_loop(renderLoop, 9999, 0);
 #else
-  while (1) { renderLoop(&args); }
+  while (1) { renderLoop(); }
 #endif
 
 }
 
-static void renderLoop(struct args* args)
+static void renderLoop()
 {
-    struct Chip8* chip8 = args->chip8;
-    SDL_Renderer* renderer = args->renderer;
-
     SDL_Event event;
     while (SDL_PollEvent(&event))
     {
       switch (event.type)
       {
         case SDL_QUIT:
+#ifdef EMSCRIPTEN
+          emscripten_cancel_main_loop();
+#else
           exit(0);
+#endif
         break;
 
         case SDL_KEYDOWN:
         {
           char key = event.key.keysym.sym;
-          char vkey = chip8_keyboard_map(&chip8->keyboard, key);
+          char vkey = chip8_keyboard_map(&chip8.keyboard, key);
           if (vkey != -1) {
-            chip8_keyboard_down(&chip8->keyboard, vkey);
+            chip8_keyboard_down(&chip8.keyboard, vkey);
           }
         }
         break;
@@ -137,9 +123,9 @@ static void renderLoop(struct args* args)
         case SDL_KEYUP:
         {
           char key = event.key.keysym.sym;
-          char vkey = chip8_keyboard_map(&chip8->keyboard, key);
+          char vkey = chip8_keyboard_map(&chip8.keyboard, key);
           if (vkey != -1) {
-            chip8_keyboard_up(&chip8->keyboard, vkey);
+            chip8_keyboard_up(&chip8.keyboard, vkey);
           }
         }
         break;
@@ -155,7 +141,7 @@ static void renderLoop(struct args* args)
     {
       for (int y = 0; y < CHIP8_HEIGHT; y++) 
       {
-        if (chip8_screen_is_set(&chip8->screen, x, y)) 
+        if (chip8_screen_is_set(&chip8.screen, x, y)) 
         {
           SDL_Rect r;
           r.x = x * CHIP8_WINDOW_SCALE;
@@ -170,19 +156,20 @@ static void renderLoop(struct args* args)
 
     SDL_RenderPresent(renderer);
 
-    if (chip8->registers.delay_timer > 0) {
+    if (chip8.registers.delay_timer > 0) {
       /* usleep(100); */
-      chip8->registers.delay_timer -= 1;
+      chip8.registers.delay_timer -= 1;
     }
 
-    if (chip8->registers.sound_timer > 0) {
-      toot(2000, 20 * chip8->registers.sound_timer);
-      chip8->registers.sound_timer = 0;
+    if (chip8.registers.sound_timer > 0) {
+      toot(2000, 20 * chip8.registers.sound_timer);
+      chip8.registers.sound_timer = 0;
     }
 
-    unsigned short opcode = chip8_memory_get_short(&chip8->memory, chip8->registers.PC);
+    unsigned short opcode = chip8_memory_get_short(&chip8.memory, chip8.registers.PC);
 
-    chip8->registers.PC += 2;
+    chip8.registers.PC += 2;
 
-    chip8_exec(chip8, opcode);
+    chip8_exec(&chip8, opcode);
+
 }
